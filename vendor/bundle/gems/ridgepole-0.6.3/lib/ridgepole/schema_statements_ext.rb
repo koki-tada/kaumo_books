a4b3d2920269150fab42a8e536b67397984c3231
rensei <myvd.renu@gmail.com>
2016-01-16 16:43:52 +0900

    add gitignore

--- vendor/bundle/gems/ridgepole-0.6.3/lib/ridgepole/schema_statements_ext.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record/connection_adapters/abstract/schema_statements'
-
-module ActiveRecord::ConnectionAdapters::SchemaStatements
-  def rename_table_indexes_with_ignore(table_name, new_name)
-    # Nothing to do
-  end
-
-  alias_method_chain :rename_table_indexes, :ignore
-end
